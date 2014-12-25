00068d0d59c6f0fcf1625c09867c8b835ce38a9a
Joel Verhagen <joel.verhagen@gmail.com>
2014-12-25 10:08:41 -0800

    Add NinjectExtensions

---
+++ Knapcode.KitchenSink/Extensions/NinjectExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Ninject.Activation;
+using Ninject.Infrastructure.Language;
+using Ninject.Syntax;
+
+namespace Knapcode.KitchenSink.Extensions
+{
+    public static class NinjectExtensions
+    {
+        public static IBindingInNamedWithOrOnSyntax<T> WhenInjectedIntoDescendentOf<T>(this IBindingWhenSyntax<T> when, Type ancestor)
+        {
+            return when.When(r => IsTypeRequestedByAncestor(r, ancestor));
+        }
+
+        private static bool IsTypeRequestedByAncestor(IRequest r, Type ancestor)
+        {
+            while (r != null && !IsTypeInRequest(r, ancestor))
+            {
+                r = r.ParentRequest;
+            }
+
+            return r != null;
+        }
+
+        private static bool IsTypeInRequest(IRequest r, Type parent)
+        {
+            if (parent.IsGenericTypeDefinition)
+            {
+                if (parent.IsInterface)
+                {
+                    return r.Target != null &&
+                           r.Target.Member != null &&
+                           r.Target.Member.ReflectedType != null &&
+                           r.Target.Member.ReflectedType.GetInterfaces().Any(i =>
+                               i.IsGenericType &&
+                               i.GetGenericTypeDefinition() == parent);
+                }
+
+                return
+                    r.Target != null &&
+                    r.Target.Member.ReflectedType.GetAllBaseTypes().Any(i =>
+                        i.IsGenericType &&
+                        i.GetGenericTypeDefinition() == parent);
+            }
+
+            return r.Target != null && parent.IsAssignableFrom(r.Target.Member.ReflectedType);
+        }
+    }
+}
