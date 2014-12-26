3c317327fe24b4acb6ce8a15365e98a94dbae60a
Joel Verhagen <joel.verhagen@gmail.com>
2014-12-25 18:48:14 -0800

    Add TryGetValue

---
+++ Knapcode.KitchenSink/Extensions/UniversalExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Knapcode.KitchenSink.Extensions
+{
+    public static class UniversalExtensions
+    {
+        public delegate bool TryGetValue<in TInput, TOutput>(TInput input, out TOutput value);
+
+        public  static TOutput GetTryValue<TInput, TOutput>(this TInput input, TryGetValue<TInput, TOutput> tryGetValue)
+        {
+            TOutput value;
+            if (!tryGetValue(input, out value))
+            {
+                value = default(TOutput);
+            }
+
+            return value;
+        }
+    }
+}
