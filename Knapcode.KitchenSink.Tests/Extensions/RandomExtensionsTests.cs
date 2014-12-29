ce6a34934a6e4076bf7ce42da6af9df468edb1e1
Joel Verhagen <joel.verhagen@gmail.com>
2014-12-28 19:58:50 -0800

    Add NextTimeSpan, NextDateTime, and NextDateTimeOffset

---
+++ Knapcode.KitchenSink.Tests/Extensions/RandomExtensionsTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using FluentAssertions;
+using Knapcode.KitchenSink.Extensions;
+using Knapcode.KitchenSink.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Knapcode.KitchenSink.Tests.Extensions
+{
+    [TestClass]
+    public class RandomExtensionsTests
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void NextTimeSpan_WithMaximumLessThanMinimum_ThrowsException()
+        {
+            // ARRANGE, ACT
+            Action action = () => RandomProvider.GetThreadRandom().NextTimeSpan(TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(1));
+
+            // ASSERT
+            action.ShouldThrow<ArgumentException>().And.ParamName.Should().Be("maximum");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextTimeSpan_WithSameMaximumAsMinimum_Succeeds()
+        {
+            // ARRANGE
+            TimeSpan expected = TimeSpan.FromMinutes(1);
+
+            // ARRANGE, ACT
+            var actual = RandomProvider.GetThreadRandom().NextTimeSpan(expected, expected);
+
+            // ASSERT
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextTimeSpan_WithDifferentMaximumAndMinimum_Succeeds()
+        {
+            // ARRANGE
+            TimeSpan minimum = TimeSpan.FromDays(0);
+            TimeSpan maximum = TimeSpan.FromDays(1);
+
+            // ARRANGE, ACT
+            var actual = RandomProvider.GetThreadRandom().NextTimeSpan(minimum, maximum);
+
+            // ASSERT
+            actual.Should().BeGreaterOrEqualTo(minimum);
+            actual.Should().BeLessOrEqualTo(maximum);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextDateTime_WithMaximumLessThanMinimum_ThrowsException()
+        {
+            // ARRANGE, ACT
+            Action action = () => RandomProvider.GetThreadRandom().NextDateTime(new DateTime(2000, 1, 1), new DateTime(1999, 1, 1));
+
+            // ASSERT
+            action.ShouldThrow<ArgumentException>().And.ParamName.Should().Be("maximum");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextDateTime_WithSameMaximumAsMinimum_Succeeds()
+        {
+            // ARRANGE
+            var expected = new DateTime(2000, 1, 1);
+
+            // ARRANGE, ACT
+            var actual = RandomProvider.GetThreadRandom().NextDateTime(expected, expected);
+
+            // ASSERT
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextDateTime_WithDifferentMaximumAndMinimum_Succeeds()
+        {
+            // ARRANGE
+            var minimum = new DateTime(1999, 1, 1);
+            var maximum = new DateTime(2000, 1, 1);
+
+            // ARRANGE, ACT
+            var actual = RandomProvider.GetThreadRandom().NextDateTime(minimum, maximum);
+
+            // ASSERT
+            actual.Should().BeOnOrAfter(minimum);
+            actual.Should().BeOnOrBefore(maximum);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextDateTimeOffset_WithMaximumLessThanMinimum_ThrowsException()
+        {
+            // ARRANGE, ACT
+            Action action = () => RandomProvider.GetThreadRandom().NextDateTimeOffset(new DateTimeOffset(new DateTime(2000, 1, 1)), new DateTimeOffset(new DateTime(1999, 1, 1)));
+
+            // ASSERT
+            action.ShouldThrow<ArgumentException>().And.ParamName.Should().Be("maximum");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextDateTimeOffset_WithSameMaximumAsMinimum_Succeeds()
+        {
+            // ARRANGE
+            var expected = new DateTimeOffset(new DateTime(2000, 1, 1));
+
+            // ARRANGE, ACT
+            var actual = RandomProvider.GetThreadRandom().NextDateTimeOffset(expected, expected);
+
+            // ASSERT
+            actual.Should().Be(expected);
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void NextDateTimeOffset_WithDifferentMaximumAndMinimum_Succeeds()
+        {
+            // ARRANGE
+            var minimum = new DateTimeOffset(new DateTime(1999, 1, 1));
+            var maximum = new DateTimeOffset(new DateTime(2000, 1, 1));
+
+            // ARRANGE, ACT
+            var actual = RandomProvider.GetThreadRandom().NextDateTimeOffset(minimum, maximum);
+
+            // ASSERT
+            actual.Should().BeOnOrAfter(minimum);
+            actual.Should().BeOnOrBefore(maximum);
+        }
+    }
+}
---
+++ Knapcode.KitchenSink/Extensions/RandomExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Knapcode.KitchenSink.Extensions
+{
+    public static class RandomExtensions
+    {
+        public static TimeSpan NextTimeSpan(this Random random, TimeSpan minimum, TimeSpan maximum)
+        {
+            if (maximum < minimum)
+            {
+                throw new ArgumentException("The provided maximum TimeSpan is less than the minimum.", "maximum");
+            }
+
+            double ticks = random.NextDouble()*(maximum - minimum).Ticks;
+            var roundedTicks = (long) Math.Round(ticks, 0);
+            return minimum + TimeSpan.FromTicks(roundedTicks);
+        }
+
+        public static DateTime NextDateTime(this Random random, DateTime minimum, DateTime maximum)
+        {
+            return minimum + random.NextTimeSpan(TimeSpan.Zero, maximum - minimum);
+        }
+
+        public static DateTimeOffset NextDateTimeOffset(this Random random, DateTimeOffset minimum, DateTimeOffset maximum)
+        {
+            return minimum + random.NextTimeSpan(TimeSpan.Zero, maximum - minimum);
+        }
+    }
+}
